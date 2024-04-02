4f3588ef6585fe9bf8ef8ba97d6c943c539522bd
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-04-01 21:48:11 -0500

    added content to about page.js

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello, My name is Laqwanda.</h1>
+      <p>
+        The quote from the previous page was by Charlie Chaplin. It one of my
+        favorite quotes.
+      </p>
+    </main>
+  );
+}
